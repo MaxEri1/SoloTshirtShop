5682bc1352356769ba950b6dc642b7e35cef2818
MaxEri1 <maxxen.me@gmail.com>
2019-10-21 14:57:40 +0200

    tester för rest-routes enligt thomas test-runner

--- rest-queries/02-check-no-tshirts.js
+++ rest-queries/02-check-no-tshirts.js
@@ -1,6 +1,6 @@
 // Note: Using assert test syntax
 module.exports = ({ assert, response, store }) => ({
-  path: 'users',
+  path: 'tshirt',
   method: 'get',
   test() {
    // Check that there are no users in DB
@@ -8,6 +8,6 @@ module.exports = ({ assert, response, store }) => ({
   },
   setup() {
     // Store the mockusers in the db (setup for next step/query)
-    store.mockUsers = require('./mock-users.json');
+    store.mockTshirt = require('./mock-tshirt.json');
   }
 });
--- rest-queries/04-check-all-tshirts-inserted.js
+++ rest-queries/04-check-all-tshirts-inserted.js
@@ -1,13 +1,13 @@
 // Note: Using assert test syntax
 module.exports = ({ assert, response, store }) => ({
-  path: 'users',
+  path: 'tshirt',
   method: 'get',
   test() {
     // check that we got the right number of users
     // (intentionelly spelled length wrong to provoke a fail)
-    assert.equal(response.length, store.mockUsers.length);
+    assert.equal(response.length, store.mockTshirt.length);
     // check that all users where
     // inserted correctly with correct data
-    assert.deepEqual(response, store.mockUsers);
+    assert.deepEqual(response, store.mockTshirt);
   }
 });
